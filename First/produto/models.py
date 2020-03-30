8e9e42b0ee9e692f4afe21f048708764b68555f2
SrKenny <kennydt.andersson@gmail.com>
2020-03-30 13:06:32 -0300

    Alteração de permissões
    "Realizado a alteração de permissões e a realação dos modelos."

--- First/produto/models.py
+++ First/produto/models.py
@@ -5,14 +5,22 @@ from django.utils import timezone
 
 # Modelo Categoria
 
+<<<<<<< Updated upstream:First/produto/models.py
 class Categories(models.Model):  
+=======
+class Categories(models.Model):
+>>>>>>> Stashed changes:Documents/Commerce/Commerce_Teste/First/produto/models.py
     label = models.SlugField(max_length=50, unique=True, blank=True, null=True)
     type = models.CharField(max_length=50)
     rank = models.IntegerField()
     image = models.ImageField()
     timestamp = models.DateTimeField(default=timezone.now)
     date_update = models.DateTimeField(blank=True, null=True)
+<<<<<<< Updated upstream:First/produto/models.py
     categoria = []
+=======
+
+>>>>>>> Stashed changes:Documents/Commerce/Commerce_Teste/First/produto/models.py
     
     #Função para apresentar o nome da categoria     
 
@@ -21,7 +29,11 @@ class Categories(models.Model):
 
 # Modelo Produto 
 class Products(models.Model):
+<<<<<<< Updated upstream:First/produto/models.py
     category = Categories.label
+=======
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+>>>>>>> Stashed changes:Documents/Commerce/Commerce_Teste/First/produto/models.py
     title = models.CharField(max_length=100)
     name = models.CharField(max_length=50)
     description = models.TextField()
